256141051064c9cf97e80e573ea7cfea6f10eb99
GMVeer <gmveer1205@gmail.com>
2022-02-05 18:08:14 +0530

    Lets start

---
+++ src/com/gitnew/GitStart.java
@@ -0,0 +1,22 @@
+package com.gitnew;
+
+public class GitStart {
+
+	private void add() {
+System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		GitStart git = new GitStart();
+		git.add();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
